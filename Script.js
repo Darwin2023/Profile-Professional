a70df108611b9d5b767a56729bef9474660e25af
Darwin2023 <149642007+Darwin2023@users.noreply.github.com>
2024-03-02 12:23:28 -0500

    Proyecto Final

---
+++ Script.js
@@ -0,0 +1,14 @@
+document.getElementById('nombre').innerText = 'DARWIN OSWALDO OSPINA CARRANZA';
+document.getElementById('profesion').innerText = 'Desarrollador Full Stack';
+document.getElementById('correo').innerText = 'Correo: ospinadarwin@gmail.com';
+document.getElementById('telefono').innerText = 'Teléfono: (57) 3203775390';
+document.getElementById('grado-educacion').innerText = 'Tecnico Sistematizacion de Datos';
+document.getElementById('universidad').innerText = 'TALENTO TECH COLOMBIA';
+document.getElementById('empresa').innerText = 'BANCO COOPERATIVO COOPCENTRAL';
+document.getElementById('puesto').innerText = 'Desarrollador Frontend'; 
+document.getElementById('fechas').innerText = 'Febrero 2020 - Presente';
+document.getElementById('logros').innerHTML = '<li>“Colaboré en el desarrollo y programación de 8 aplicaciones..”</li><li>"implemente un sistema que mejoró la eficiencia del departamento en un 20%.”</li>';
+// Puedes agregar más actualizaciones dinámicas según tus necesidades.
+
+
+
